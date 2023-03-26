95ad7f2edacf22b922180255a6f993d84f5ab3d7
myfootsasleep <richardoda@gmail.com>
2023-03-26 16:13:08 -0700

    Implement Tail

---
+++ tail.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+let newTail = [];
+const tail = function(arr1) {
+  newTail = arr1.slice(1);
+  return newTail;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
