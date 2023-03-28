50d67052ed878eed2efe1233518803abe732a490
myfootsasleep <richardoda@gmail.com>
2023-03-28 01:25:20 -0700

    Fixed my code, it wasn't working the way I wanted it at first

--- eqArrays.js
+++ eqArrays.js
@@ -6,15 +6,22 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-let eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
+let eqArrays = function(arr1, arr2){
+  let testResult = false
+  if (arr1.length === arr2.length){
     //console.log("true")
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[2]) {
-        return true;
+    for (let i = 0; i < arr1.length; i++){
+      console.log(arr1[i], arr2[i])
+      if (arr1[i] !== arr2[i]){
+        testResult = false
+        return false
+      }
+      else{
+        testResult = true
       }
     }
   }
-};
+  return testResult
+}
 
-assertEqual(eqArrays([1, 2, 3], [1, 2,3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
