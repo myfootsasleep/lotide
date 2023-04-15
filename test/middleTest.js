ae4dc19d75016e6158b6c3f05a05109f8074fa81
myfootsasleep <richardoda@gmail.com>
2023-04-15 14:56:05 -0700

    adding test files to the git

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,23 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
-assertArraysEqual((middle([1])), []);
+describe("#middle", () =>{
+  it("Should Return the middle values of the array []", () => {
+    assert.deepEqual((middle([1])), []);
+  });
+  it("Should Return the middle values of the array [2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("Should Return the middle values of the array [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
+
+
 //console.log(middle([1]).length)
 // middle([1, 2]) // => []
 // middle([1, 2, 3]) // => [2]
 // middle([1, 2, 3, 4, 5]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 // middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
 
 // let nani = []
--- test/tailsTest.js
+++ test/tailsTest.js
@@ -1,14 +1,9 @@
 const tail = require('../tail');
-const assertEqual = (require("../assertEqual"));
 const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
 describe("#tail", () =>{
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(tail([1, 2, 3]), 1);
-  });  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+  it("Should Return the array without the first value", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse","Labs"]), ["Lighthouse","Labs"]);
   });
 });
 
