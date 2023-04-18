b1f2abd287c24598e67d718e9a926d059dad6a98
myfootsasleep <richardoda@gmail.com>
2023-04-17 23:27:08 -0700

    fixed the without.js

--- without.js
+++ without.js
@@ -3,7 +3,7 @@ let without = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     for (let x = 0; x < arr2.length; x++) {
       if (arr1[i] === arr2[x]) {
-        copyArr.splice(i, 1);
+        copyArr.splice(i);
       }
     }
   }
