5ad974e204bdcd0bf824de377df4403d7b645417
myfootsasleep <richardoda@gmail.com>
2023-04-17 23:22:43 -0700

    refractored more of the code, removed unnecessary test code

--- findKey.js
+++ findKey.js
@@ -1,29 +1,9 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 const findKey = function(objects, callback) {
-  newObj = {};
-  for (stars in objects) {
+  for (let stars in objects) {
     //console.log(objects[stars])
     if (callback(objects[stars])) {
       return stars;
     }
   }
 };
-
-// testObj = findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2);
-
-// assertEqual((testObj), "noma");
 module.exports = findKey;
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,6 +1,5 @@
 let findKeyByValue = function(object1, key) {
   for (let movies in object1) {
-    //console.log(object1[movies])
     if (object1[movies] === key) {
       return movies;
     }
