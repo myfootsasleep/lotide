1e4c658ff378ffb41ffd7f85a3e399b00a06538e
myfootsasleep <richardoda@gmail.com>
2023-04-17 22:30:10 -0700

    added missing require files

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const letterPosition = require('./letterPosition');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
 
 module.exports = {
   head,
   tail,
   middle,
+  flatten,
+  countOnly,
+  letterPosition,
+  findKeyByValue,
+  eqObjects,
+  eqArrays
 };
